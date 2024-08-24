0532cf9e2e6b3725ce938a04fcb5300cdcfb6c9a
st136386207 <147305886+st136386207@users.noreply.github.com>
2024-08-24 13:02:27 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -101,7 +101,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["ds.mscon.cc"];
-pub const PUBLIC_RS_PUB_KEY: &str = "htHAWCrWQICi09WffPTYJjavzYKG6YiqM7agOR7cGC0=";
+pub const PUBLIC_RS_PUB_KEY: &str = "";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
