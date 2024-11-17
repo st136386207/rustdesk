bfd0cb0c9811f3e820c0b686f03bfa3687fa6873
st136386207 <147305886+st136386207@users.noreply.github.com>
2024-11-17 21:38:12 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["ds.mscon.cc"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["ds.cluhd.top"];
 pub const PUBLIC_RS_PUB_KEY: &str = "qwhPG1PZmICRWFGM1GHGz7YQ9z6bdiAy5GGe7v21Jp2vsA=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
